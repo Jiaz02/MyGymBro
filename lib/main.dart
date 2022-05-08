808d7bd1e4b949ecc6899a74ceb1a2e52eb82426
Jiaz02 <jsaavedraa@iesch.org>
2022-05-08 20:52:51 +0200

    funciona sin errores

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:my_gym_bro/router/app_routes.dart';
 import 'package:my_gym_bro/theme/app_theme.dart';
 
-//
-
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
