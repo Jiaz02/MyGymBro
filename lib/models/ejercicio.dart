29705a22b317154d2789c5e09e84aff8e15e43c4
jsaavedraalquezar <jsaavedraalquezar>
2022-05-19 17:46:03 +0200

    creo la clase

---
+++ lib/models/ejercicio.dart
@@ -0,0 +1,11 @@
+class Ejercicio {
+    Ejercicio({
+        required this.name,
+        required this.description,
+        required this.muscle,
+    });
+
+    String name;
+    String description;
+    List<String> muscle;
+}
