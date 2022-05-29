cc79db0527bb33f79342e6190cd06eca72aa8d4a
Jiaz02 <jsaavedraa@iesch.org>
2022-05-29 20:38:19 +0200

    hn

--- lib/screens/ejercicios_screen.dart
+++ lib/screens/ejercicios_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:my_gym_bro/router/app_routes.dart';
 import 'package:my_gym_bro/theme/app_theme.dart';
 import 'package:my_gym_bro/widgets/widgets.dart';
@@ -9,11 +10,10 @@ import 'package:http/http.dart' as http;
 import '../models/models.dart';
 
 Future<Ejercicio> fetchEjercicio() async {
-  final response =
-      await http.get(Uri.parse('/assets/ejercicios.json'));
+  final response = await http.get(Uri.parse('/assets/ejercicios.json'));
 
   if (response.statusCode == 200) {
-    print(" contenido "+response.body);
+    print(" contenido " + response.body);
     final jsonm = "[" + response.body + "]";
     // Si la llamada al servidor fue exitosa, analiza el JSON
     return Ejercicio.fromJson(json.decode(jsonm));
@@ -24,24 +24,24 @@ Future<Ejercicio> fetchEjercicio() async {
 }
 
 class EjerciciosScreen extends StatelessWidget {
- EjerciciosScreen({Key? key}) : super(key: key);
+  EjerciciosScreen({Key? key}) : super(key: key);
 
   Future<Ejercicio> ejercicio = fetchEjercicio();
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<Ejercicio>(
-            future: ejercicio,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data!.name);
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
+      future: ejercicio,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Text(snapshot.data!.name);
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
 
-              // Por defecto, muestra un loading spinner
-              return CircularProgressIndicator();
-            },
-          );
+        // Por defecto, muestra un loading spinner
+        return CircularProgressIndicator();
+      },
+    );
   }
 }
