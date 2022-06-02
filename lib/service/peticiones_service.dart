f88f0a1b4011dd27fab5497b57ac3d9d189a5dd1
jsaavedraalquezar <jsaavedraalquezar>
2022-06-02 17:08:19 +0200

    nc dl svc

--- lib/service/peticiones_service.dart
+++ lib/service/peticiones_service.dart
@@ -1,18 +1,6 @@
-// import 'dart:convert';
+import 'package:flutter/material.dart';
 
-// import 'package:http/http.dart' as http;
+class RutinaService extends ChangeNotifier {
+  
+}
 
-// class PeticionesService extends ChangeNotifier{
-//   Future<http.Response> fetchPost() async {
-//     var response =
-//         await http.get(Uri.parse('/assets/ejercicios.json/ejercicio'));
-//     if (response.statusCode == 200) {
-//       var jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
-//       var itemCount = jsonResponse['totalItems'];
-//       print('Number of books about http: $itemCount.');
-//     } else {
-//       print('Request failed with status: ${response.statusCode}.');
-//     }
-//     return http.get(Uri.parse('/assets/ejercicios.json/ejercicio'));
-//   }
-// }
