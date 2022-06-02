a97d65984a9e931a4f81c5323f346ad9a9730f0d
jsaavedraalquezar <jsaavedraalquezar>
2022-06-02 17:36:50 +0200

    wrtyuiop

--- lib/service/peticiones_service.dart
+++ lib/service/peticiones_service.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 
 class RutinaService extends ChangeNotifier {
   
+final String _baseUrl='mygymbro-f072a-default-rtdb.europe-west1.firebasedatabase.app';
+
 }
 
